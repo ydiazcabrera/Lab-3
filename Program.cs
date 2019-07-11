47d6a922979079a395d054da0202497df793950d
Yeni <yen.diaz@hotmail.com>
2019-07-11 17:36:04 -0400

    refactor

--- Program.cs
+++ Program.cs
@@ -11,56 +11,61 @@ namespace Lab_3
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter your name:");
-            string name= Console.ReadLine();
-            while(true)
-            {
-                 Console.WriteLine(name + ", enter a number between 1 and 100.");
-                 int input = int.Parse(Console.ReadLine());
+            string name = Console.ReadLine();
+          while (true)
+          {
+                Console.WriteLine($"{name}, enter a number between 1 and 100.");
+                int input = int.Parse(Console.ReadLine());
 
-                 if (input <1 || input >100)
-                 {   
-                    Console.WriteLine("Input not valid. Only numbers from 1 to 100 accepted.");
+             if (input < 1 || input > 100)
+             {
+                Console.WriteLine("Input not valid. Only numbers from 1 to 100 accepted.");
+                continue;
+             }
+                if (input % 2 != 0)
+                {                    
+                    if (input > 60)
+                    {
+                        Console.WriteLine($"{name}, the number {input} is Odd");
+                    }
+                    else if (input < 100)
+                    {
+                        Console.WriteLine("Odd");
+                    }
+                }
+
+                if (input % 2 == 0)
+                {
+                    if (input <= 25 && input >= 2)
+                    {
+                        Console.WriteLine($"{name}, the number is even and less than 25.");
+                    }
+                    else if (input <= 60 && input >= 26)
+                    {
+                        Console.WriteLine($"{name}, the number is Even");
+                    }
+                    else if (input > 60)
+                    {
+                        Console.WriteLine($"{name}, the number {input} is Even");
+                    }
+                }
+
+
+            Console.WriteLine("Do you wish to conitue?  (Y/N)");
+            string answer = Console.ReadLine().ToLower();
+
+                if (answer == "y" || answer == "yes")
+                {
                     continue;
-                 }
-                                                                     
-                 if (input % 2 != 0)
-                 {
-                    Console.WriteLine(name + ", the number " + input + " is "  + "Odd");
-                 }
-                 if(input%2 ==0 && input >= 2 && input < 25)
-                 {
-                     Console.WriteLine(name + ", the number is even and less than 25.");
-                 }
-                 if (input % 2 == 0 && input >= 26 && input <= 60)
-                 {
-                     Console.WriteLine(name + ", the number is even");
-                 }
-                 if (input % 2 == 0 && input > 60)
-                 {
-                      Console.WriteLine(name + ", the number" + input +" is " + "Even");
-                 }
-                 if ((input%2 !=0) && input > 60)
-                 {
-                    Console.WriteLine(name + ",the number " + input + " is " + "Odd");
-                 }
-
-             Console.WriteLine("Do you wish to conitue?  (Y/N)");
-             string answer= Console.ReadLine();
-                             
-                if (answer =="Y" || answer=="yes" || answer=="y")
-                {
-
                 }
-             
-                else
+                else if (answer == "n" || answer == "no")
                 {
-                  break;
-                }
-
-            }
+                    break;
+                }             
+          }
             
            Console.WriteLine("Thank you for participating");
-            Console.ReadLine();
+           Console.ReadLine();
            
         }
     }
